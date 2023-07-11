c4637f6bdc3f51d9adc0de08b8356b964926b4ab
Don Moses <githinjidon@gmail.com>
2023-07-11 11:16:29 +0300

    make changes to registration module

--- devices/urls.py
+++ devices/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    path('', views.home, name='home'),
+    path('', views.home_view, name='home'),
 ]
 
 if settings.DEBUG:
--- maintain_it/settings.py
+++ maintain_it/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'devices',
     'bootstrap4',
     'markupfield_helpers',
+    'project_home_tags',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -163,6 +164,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SYSTEM_MAINTENANCE_PAGINATE_BY = 50
+
+PROJECT_HOME_NAMESPACE = 'maintain_it:index_view'    # Namespace of homepage
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
