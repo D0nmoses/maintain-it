f705c96e82106f16202b87b58860c0da5cac6d96
Don Moses <githinjidon@gmail.com>
2023-07-11 11:31:57 +0300

    register models to admin

--- devices/admin.py
+++ devices/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, DocumentationRecord, Hardware, MaintenanceRecord, MaintenanceType, Software, SysAdmin, System
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(DocumentationRecord)
+admin.site.register(Hardware)
+admin.site.register(MaintenanceRecord)
+admin.site.register(MaintenanceType)
+admin.site.register(Software)
+admin.site.register(SysAdmin)
+admin.site.register(System)
