fe856ac08a0636c5b9f44e5ca935bd81079e5226
Don Moses <githinjidon@gmail.com>
2023-07-12 15:50:05 +0300

    add pagination functionality and related records button template

--- devices/urls.py
+++ devices/urls.py
@@ -9,9 +9,9 @@ urlpatterns=[
     path('', views.home_view, name='home'),
     re_path(r'^profile/(\d+)', views.profile, name="profile"),
     path('records/', views.MaintenanceRecordListView.as_view(), name='maintenance_record_list'),
-    path('records/<int:id>/', views.MaintenanceRecordDetailView.as_view(), name='maintenance_record_detail'),
+    path('records/<int:pk>/', views.MaintenanceRecordDetailView.as_view(), name='maintenance_record_detail'),
     path('documentation/', views.DocumentationRecordListView.as_view(), name='documentation_record_list'),
-    path('documentation/<int:id>/', views.DocumentationRecordDetailView.as_view(), name='documentation_record_detail'),
+    path('documentation/<int:pk>/', views.DocumentationRecordDetailView.as_view(), name='documentation_record_detail'),
 ]
 
 if settings.DEBUG:
